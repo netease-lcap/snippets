797cf7ba204596de8280b52aad88b17adf0c4d0b
Baymax808 <grp.lcap.fe.bot@corp.netease.com>
2026-07-09 17:08:32 +0800

    Upload views/head/system-table-prefix/render.vue - 187b189bafb875dcec965c472f698542

--- 4.5.0/views/head/system-table-prefix/render.vue
+++ 4.5.0/views/head/system-table-prefix/render.vue
@@ -15,7 +15,7 @@
         <el-form v-if="props.position === 'preference' && systemTablePrefixType === SystemTablePrefixTypeEnum.custom" :class="$style.form"
           :model="systemTablePrefix"
         >
-          <el-form-item ref="systemTablePrefixFormItem" prop="value" :rules="rules.systemTablePrefix" style="margin-bottom: 0">
+          <el-form-item ref="systemTablePrefixFormItem" prop="value" :rules="rules.systemTablePrefix">
             <el-input
               v-model="systemTablePrefix.value"
               placeholder="请输入前缀" maxlength="10"
@@ -112,7 +112,6 @@ const {
 }
 .root[position="preference"] .customRadio {
   align-items: flex-start;
-  height: auto;
 }
 .root[position="preference"] .form {
   margin-top: 6px;
