e4319745e50a49963df0c1645c5ac940b160e10b
Baymax808 <grp.lcap.fe.bot@corp.netease.com>
2026-08-28 16:38:16 +0800

    Upload views/head/export-log-modal/render.vue - 065f763631ad439f0fb008e528732a19

---
+++ 4.5.5/views/head/export-log-modal/render.vue
@@ -0,0 +1,73 @@
+<template>
+  <el-dialog
+    v-model="visible"
+    title="应用日志"
+    :class="$style.modal"
+    destroy-on-close
+    append-to-body
+    :close-on-click-modal="false"
+    @close="close"
+  >
+    <s-log
+      :events="events"
+      :flowLogs="graph"
+      :exportStatus="exportStatus"
+      :jobId="jobId"
+      :isRemoteDeploy="false"
+    />
+  </el-dialog>
+</template>
+
+<script setup>
+import { inject } from 'vue';
+
+const {
+  /**
+   * 弹窗是否可见
+   *
+   * @type {Vue.ref<boolean>}
+   */
+  visible,
+  /**
+   * 日志事件列表
+   *
+   * @type {Vue.ref<Array>}
+   */
+  events,
+  /**
+   * 任务 ID
+   *
+   * @type {Vue.ref<string>}
+   */
+  jobId,
+  /**
+   * 导出状态
+   *
+   * @type {Vue.ref<string>}
+   */
+  exportStatus,
+  /**
+   * 流程图数据
+   *
+   * @type {Vue.ref<any>}
+   */
+  graph,
+  /**
+   * 关闭弹窗并通知父组件
+   *
+   * @function
+   */
+  close,
+} = inject('$context');
+</script>
+
+<style module>
+.modal {
+  width: 1000px;
+  height: 490px;
+}
+
+.modal div[class^='el-dialog__body'] {
+  margin: 0;
+}
+</style>
